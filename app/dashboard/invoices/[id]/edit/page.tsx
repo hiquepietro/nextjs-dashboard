5a4f020326600e32cd6c0b1d2ec80625dbc4a1fc
Hique Pietro <hiquepietro@gmail.com>
2024-01-19 10:06:04 -0300

    criando a acessibilidade no formulario

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,13 +2,20 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { customers } from '@/app/lib/placeholder-data';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
 
  
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;const [invoice, customers] = await Promise.all([
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
       ]);
+
+      if (!invoice) {
+        notFound();
+      }
 
   return (
     <main>
